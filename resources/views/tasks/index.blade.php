1cdf5d5c27e482fab6fa2b97003b5c11b35c6a9f
qiuxan <qiuxan@gmail.com>
2018-10-18 14:43:02 +1100

    db data capture

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+<body>
+
+<ul>
+    @foreach($tasks as $task)
+
+        <li>{{$task->body}}</li>
+
+    @endforeach
+</ul>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tasks', function () {
+
+    $tasks=DB::table('tasks')->get();
+//    return $tasks;
+    return view('tasks.index',compact('tasks'));
+});
+
+
+Route::get('/tasks/{tasks}', function ($id) {
+
+    $task=DB::table('tasks')->find($id);
+
+    //dd($task);
+
+    return view('tasks/show',compact('task'));
 });
