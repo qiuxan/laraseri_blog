bd82aad776d3fdd6ff2b771b7ca1c6c4e8a47f68
qiuxan <qiuxan@gmail.com>
2018-11-03 21:20:01 +1100

    archives 1

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
@@ -30,11 +31,38 @@ class PostsController extends Controller
     public function index(){
 
 //        $posts= Post::all();
-        $posts= Post::latest()->get();
+        $posts= Post::latest();
+
+        if($month=request('month')){
+
+            $posts->whereMonth('created_at',Carbon::parse($month)->month);
+
+        }
+
+        if($year=request('year')){
+
+            $posts->whereYear('created_at',$year);
+
+        }
 
 
+        $posts=$posts->get();
 
-        return view('task2.index',compact('posts'));
+//        if($year=request('year')){
+//            $posts->whereYear('created_at',$year);
+//
+//        }
+
+        $archives=Post::selectRaw('year(created_at) year,monthname(created_at) month,count(*) published')
+
+            ->groupBy('year','month')
+            ->orderByRaw('min(created_at)')
+            ->get()->toArray();
+
+        //return $archives;
+
+
+        return view('task2.index',compact('posts','archives'));
 
     }
 
