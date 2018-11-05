c5f16f42701df89f224020969121e9106f8ed264
qiuxan <qiuxan@gmail.com>
2018-11-05 11:37:47 +1100

    testing

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+
+        'user_id' => function(){
+
+            return factory(App\User::class)->create();
+        },
+
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+
+    ];
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -14,6 +17,33 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+//        $this->assertTrue(true);
+
+        $first=factory(Post::class)->create();
+
+        $second=factory(Post::class)->create(
+
+            ['created_at'=>\Carbon\Carbon::now()->subMonth()
+
+            ]);
+       $posts=Post::archives();
+
+      // dd($posts);
+
+       //$this->assertCount(2,$posts);
+
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+            ],
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+            ]
+
+            ],$posts);
     }
 }
